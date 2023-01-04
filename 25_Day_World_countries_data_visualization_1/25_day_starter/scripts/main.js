a0b87397763f655769e723ad90a92f72f27e7fc6
thienhnguyen <thienhnguyen@kms-technology.com>
2023-01-04 21:49:48 +0700

    Ex 25 Add Functions

--- 25_Day_World_countries_data_visualization_1/25_day_starter/scripts/main.js
+++ 25_Day_World_countries_data_visualization_1/25_day_starter/scripts/main.js
@@ -1,2 +1,44 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+function getAllLanguages() {
+  let languages = new Array();
+  countries_data.forEach((element) => {
+    languages = languages.concat(element["languages"]);
+  });
+
+  let languagesCountObj = {};
+  languages.forEach((lang) => {
+    languagesCountObj[lang] = (languagesCountObj[lang] || 0) + 1;
+  });
+
+  let sortable = [];
+  for (let lang in languagesCountObj) {
+    sortable.push([lang, languagesCountObj[lang]]);
+  }
+
+  return sortable.sort((a, b) => {
+    return b[1] - a[1];
+  });
+}
+
+function getPopulation() {
+  let populationArr = [];
+  let totalPopulation = 0;
+  countries_data.forEach((element) => {
+    populationArr.push([element["name"], element["population"]]);
+    totalPopulation += element["population"];
+  });
+  populationArr.push(["World", totalPopulation]);
+
+  populationArr.sort((a, b) => {
+    return b[1] - a[1];
+  });
+
+  populationArr.forEach((element) => {
+    element[1] = numberWithCommas(element[1]);
+  });
+
+  return populationArr;
+}
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+}
